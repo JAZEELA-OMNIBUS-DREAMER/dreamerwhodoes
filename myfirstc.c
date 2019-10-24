3c9e0223b3eb6a436338c1d8e92dbb351f1f1da6
JAZEELA P <55051124+JAZEELA-OMNIBUS-DREAMER@users.noreply.github.com>
2019-10-24 15:56:45 +0530

    myfirst.c
    just testing

---
+++ myfirstc.c
@@ -0,0 +1,7 @@
+    #include <stdio.h>
+    int main()
+    {
+       // printf() displays the string inside quotation
+       printf("i am jazeela this is just a testing");
+       return 0;
+    }
